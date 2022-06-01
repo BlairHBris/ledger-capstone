4f6742a27436ef3f2cfcd780bf13326f67bf2cb1
Blair Bristow <blairhbristow@gmail.com>
2022-06-01 14:06:56 -0400

    Add reuseable button component with styling and integrate into header

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent implements OnInit {
+
+  @Input() text: string | undefined;
+  @Input() color: string | undefined;
+
+  @Output() buttonClick = new EventEmitter()
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onClick() {
+    this.buttonClick.emit()
+  }
+
+}
