d5b5ca1c46af893e17e6fbf385e59f8e61a10c0c
Blair Bristow <blairhbristow@gmail.com>
2022-06-17 09:44:35 -0400

    Initialize stocks for presentation

--- src/app/components/stocks/stocks.component.ts
+++ src/app/components/stocks/stocks.component.ts
@@ -3,22 +3,22 @@ import { StocksService } from 'src/app/services/stocks.service';
 import { Stock } from 'src/app/Stock';
 
 @Component({
-  selector: 'app-news',
-  templateUrl: './stocks.component.html',
-  styleUrls: ['./stocks.component.css']
+    selector: 'app-news',
+    templateUrl: './stocks.component.html',
+    styleUrls: ['./stocks.component.css']
 })
 
 export class StocksComponent implements OnInit {
 
-  stocks: Stock[] | undefined
+    stocks: Stock[] | undefined
 
-  constructor(private stocksService: StocksService) { }
+    constructor(private stocksService: StocksService) { }
 
-  ngOnInit(): void {
-    this.stocksService.getStocks().subscribe((response: any) => {
-        this.stocks = response
-      })
-  }
+    ngOnInit(): void {
+        this.stocksService.getStocks().subscribe((response: any) => {
+            this.stocks = response
+        })
+    }
 }
 /*
 
